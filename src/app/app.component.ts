a904342a4d98703086fca099ce2d656658ebc79e
Patxi <patxi064@gmail.com>
2025-11-29 11:22:57 +0100

    Add homepage & header nav

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   imports: [
+    HeaderComponent,
     RouterOutlet
   ],
   styleUrl: './app.component.css'
